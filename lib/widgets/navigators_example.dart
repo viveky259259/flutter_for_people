e659016d18d6a527cdfcd6bc1f0d2658784ac491
Vivek Yadav <vivek@rirev.com>
2020-03-14 06:21:25 +0530

    1. Navigator widget

---
+++ lib/widgets/navigators_example.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(InitApp());
+
+class InitApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen: screen 1'),
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => DashboardScreen(),
+              settings: RouteSettings(name: "setting")));
+        },
+        child: Text("Dashboard"),
+      ),
+    );
+  }
+}
+
+class DashboardScreen extends StatefulWidget {
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Column(
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Back to login"),
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => OrderScreen(),
+                  settings: RouteSettings(name: "/order")));
+            },
+            child: Text("Order"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderScreen extends StatefulWidget {
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Order'),
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          Navigator.popUntil(context, ModalRoute.withName("/"));
+        },
+        child: Text("Login"),
+      ),
+    );
+  }
+}
